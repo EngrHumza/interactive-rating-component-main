8108fe5ebe3de9d5e21c7a733f0883e377fcc448
EngrHumza <humzaliaqat01@gmail.com>
2022-04-08 16:41:55 +0500

    Rating Functionality in Thankyou Page

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 
 function rating(){
-
-    let x=document.getElementById("one").innerText;
-    console.log(x);
+    let a=document.getElementById("one").innerText;
+    localStorage.setItem("value",a);
 }
+function rating1(){
+    let b=document.getElementById("two").innerText;
+    localStorage.setItem("value",b);
+}
+function rating2(){
+    let c=document.getElementById("three").innerText;
+    localStorage.setItem("value",c);
+
+}
+function rating3(){
+    let d=document.getElementById("four").innerText;
+    localStorage.setItem("value",d);
+
+}
+
+function rating4(){
+    let e=document.getElementById("five").innerText;
+    localStorage.setItem("value",e);
+}
+
+let y=document.getElementById("1");
+y.textContent=localStorage.getItem("value");
